473d78ac90e6d1f424d348dabb3756ec51e9a048
Aditya Hebballe <adityahebbale@gmail.com>
2025-07-04 11:42:48 +0530

    Update install and uninstall scripts

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 mkdir -p ~/.local/bin
 mv "dist/Local Learn-1.0.0.AppImage" ~/.local/bin/local-learn
 
+# Move the icon
+mkdir -p ~/.local/share/icons
+cp assets/icon.png ~/.local/share/icons/local-learn.png
+
 # Create the .desktop file
 mkdir -p ~/.local/share/applications
 cat > ~/.local/share/applications/local-learn.desktop << EOL
@@ -11,7 +15,7 @@ cat > ~/.local/share/applications/local-learn.desktop << EOL
 Name=Local Learn
 Comment=Watch your local course files in a Udemy-like interface.
 Exec=~/.local/bin/local-learn
-Icon=$(realpath assets/icon.png)
+Icon=~/.local/share/icons/local-learn.png
 Terminal=false
 Type=Application
 Categories=Education;
--- uninstall.sh
+++ uninstall.sh
@@ -6,4 +6,7 @@ rm -f ~/.local/bin/local-learn
 # Remove the .desktop file
 rm -f ~/.local/share/applications/local-learn.desktop
 
+# Remove the icon
+rm -f ~/.local/share/icons/local-learn.png
+
 echo "Local Learn has been uninstalled."
